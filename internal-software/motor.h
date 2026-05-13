53fa61c4c8edc87927a3b89bbd26eed61ed1bf57
aylin0614 <aylinlopez614@gmail.com>
2026-05-13 11:27:23 -0700

    updated version

--- internal-software/motor.h
+++ internal-software/motor.h
@@ -1,7 +1,26 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-void motor_init(int left_pin, int right_pin);
+#include <stdint.h>
+
+// Initialize motor system
+// left/right throttle pins = PWM outputs (TH pins)
+// left/right dir pins      = direction control (RW pins)
+// left/right en pins       = enable pins (EN pins)
+void motor_init(
+    int left_throttle_pin,
+    int right_throttle_pin,
+    int left_dir_pin,
+    int right_dir_pin,
+    int left_en_pin,
+    int right_en_pin
+);
+
+void motor_enable(void);
+
+void motor_disable(void);
+
+
 void motor_set_speed(float linear, float angular);
 
 #endif
